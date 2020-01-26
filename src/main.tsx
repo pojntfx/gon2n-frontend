4b4172a5d8cae411cf373aaaefa8d4e1a973deba
Felix Pojtinger <felix@pojtinger.com>
2020-01-26 23:38:24 +0100

    feat: Add minimal listing of available supernodes

--- src/main.tsx
+++ src/main.tsx
@@ -3,4 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import "semantic-ui-css/semantic.min.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <App endpoint="http://localhost:8080" />,
+  document.getElementById("root")
+);
